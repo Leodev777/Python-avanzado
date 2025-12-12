07ddf17f00c72a708fe30d43a3405bd36b7ee39a
Leonardogit <leonardoiglesias74@gmail.com>
2025-12-12 17:06:45 -0300

    T1 C4

--- Tema 1 - Funciones/4 Practica Valores de Retorno y Módulos/ventas.py
+++ Tema 1 - Funciones/4 Practica Valores de Retorno y Módulos/ventas.py
@@ -1,1 +1,16 @@
 # Nuestra estructura sera una lista de deiccionarios
+# [{producto: "nombre producto", precio:}
+# {producto: "nombre producto", precio: precio
+#.
+
+ventas = []
+
+def agregar_venta (producto, precio): # ---> datos de entrada
+    """Agregamos la ventas a la base de datos"""
+    venta = {"producto": producto, # ---> guardamos los datos de entrada en un diccionario PRODUCTO
+             "precio": precio} # ---> guardamos los datos de entrada en un diccionario PRECIO
+    ventas.append(venta) # ---> agregamos una nueva lista y vamos guardanto las ventas con .append (sirve para agregar elementos a una lista)
+
+def mostrar_ventas(): # ---> funcion para mostrar las ventas
+    for venta in ventas:
+        print(f"producto: {venta['producto']}, precio: {venta['precio']}") # ---> mostramos el producto y el precio de cada venta
