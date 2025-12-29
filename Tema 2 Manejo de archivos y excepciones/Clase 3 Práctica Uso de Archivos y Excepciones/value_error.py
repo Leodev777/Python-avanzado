48708aa0d46cecac2ba4598ea03885797c1eddd3
Leonardogit <leonardoiglesias74@gmail.com>
2025-12-29 18:03:50 -0300

    T2 C3

---
+++ Tema 2 Manejo de archivos y excepciones/Clase 3 Práctica Uso de Archivos y Excepciones/value_error.py
@@ -0,0 +1,16 @@
+# Envolver en while infinito - brack
+# Pedir dos numeros al usuario
+# Controlar el error: str en vez de int
+# Sumar 2 numeros
+
+try:
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    result = num1 + num2 
+    print("El resultado es ", result)
+    
+
+except ValueError:
+    print("Solo numeros enteros")
+
+
